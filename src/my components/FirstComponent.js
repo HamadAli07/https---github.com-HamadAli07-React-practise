8a0e532e8f08b66e1f80b37354c53a85ef74ed44
Hamad Ali <hammadch488@gmail.com>
2023-03-26 03:30:07 +0500

    First Commit

---
+++ src/my components/FirstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>
+        <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+    </div>
+  )
+}
+
+export default FirstComponent;
---
+++ src/my components/Gallery.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import FirstComponent from './FirstComponent'
+
+const Gallery = () => {
+  return (
+    <div>
+        <section>
+      <h1>Amazing scientists</h1>
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </section>
+    </div>
+  )
+}
+
+export default Gallery
